1e6ab0d3ee5ce825f5297e0096689c72b2984b67
Jell <jean-louis@reify.se>
2025-08-21 11:39:41 +0200

    Fix: add documentation to please rubocop

--- lib/activerecord-pg-format-db-structure/preprocessors/remove_restrict_pragmas.rb
+++ lib/activerecord-pg-format-db-structure/preprocessors/remove_restrict_pragmas.rb
@@ -4,6 +4,8 @@ require_relative "base"
 
 module ActiveRecordPgFormatDbStructure
   module Preprocessors
+    # Remove \restrict and \unrestrict pragmas added by newer versions
+    # of pg_dump
     class RemoveRestrictPragmas < Base
       def preprocess!
         source.gsub!(/^(\\restrict .*)$/, "")
